330f8cbbbc1f5ceda6786750b078d80f2722b785
Nathan Wise <nathanwise@gmail.com>
2014-09-15 09:13:38 -0400

    Initial Commit

---
+++ ProtienTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProtienTracker
+//
+//  Created by Nathan Wise on 9/15/14.
+//  Copyright (c) 2014 WiseIdeas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
